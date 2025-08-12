9709060f73bcade41c1a7742f33dddb7c120c9d5
Indra Sah Noeldy <twinightwheel50@gmail.com>
2025-08-12 07:54:28 +0700

    add /casino command

---
+++ src/main/java/com/indra87g/commands/CasinoCommand.java
@@ -0,0 +1,67 @@
+package com.indra87g.commands;
+
+import cn.nukkit.Player;
+import me.onebone.economyapi.EconomyAPI;
+
+import java.util.Random;
+
+public class CasinoCommand extends BaseCommand {
+
+    public CasinoCommand() {
+        super("casino", "Random money bets", "/casino <amount>", "waffle.casino");
+    }
+
+    @Override
+    protected boolean validateArgs(String[] args, Player player) {
+        if (args.length != 1) {
+            player.sendMessage("§cWrong usage!");
+            player.sendMessage("§eUsage: " + getUsage());
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected boolean handleCommand(Player player, String[] args) {
+        int amount;
+
+        try {
+            amount = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            player.sendMessage("§cThe bet amount must be a number!");
+            return false;
+        }
+
+        if (amount <= 0) {
+            player.sendMessage("§cThe bet amount must be more than 0! .");
+            return false;
+        }
+
+        EconomyAPI economy = EconomyAPI.getInstance();
+        double balance = economy.myMoney(player);
+
+        if (balance < amount) {
+            player.sendMessage("§cYour money is not enough. Balance: §f" + balance);
+            return false;
+        }
+
+        economy.reduceMoney(player, amount);
+        player.sendMessage("§7The betting begins...");
+
+        Random rand = new Random();
+        int roll = rand.nextInt(100); // 0 - 99
+
+        if (roll < 40) { // 40% kalah
+            player.sendMessage("§cToo bad, you lost. Your money is gone :) ");
+        } else if (roll < 70) { // 30% balik modal
+            economy.addMoney(player, amount);
+            player.sendMessage("§eYou're a novice gambler, you'd better not play!");
+        } else { // 30% menang
+            int winAmount = amount * 2;
+            economy.addMoney(player, winAmount);
+            player.sendMessage("§aCongratulations! You won and got §f" + winAmount + "§a!");
+        }
+
+        return true;
+    }
+}
