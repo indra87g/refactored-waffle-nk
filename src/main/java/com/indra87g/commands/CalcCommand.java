2150514f2e99084e38dc8eb154f0cad83aab0e03
Indra Sah Noeldy <twinightwheel50@gmail.com>
2025-08-15 07:25:46 +0700

    add /calc command

---
+++ src/main/java/com/indra87g/commands/CalcCommand.java
@@ -0,0 +1,62 @@
+package com.indra87g.commands;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.CommandSender;
+
+public class CalcCommand extends BaseCommand {
+
+    public CalcCommand() {
+        super("calc", "Do a mathematical calculations ", "/calc <num1> <operator> <num2>");
+        this.setPermission("waffle.calc");
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if (!testPermission(sender)) {
+            return true;
+        }
+
+        if (args.length != 3) {
+            sender.sendMessage("§cUsage: /calc <num1> <operator> <num22>");
+            return true;
+        }
+
+        double num1, num2;
+        String operator = args[1];
+        double result;
+
+        try {
+            num1 = Double.parseDouble(args[0]);
+            num2 = Double.parseDouble(args[2]);
+        } catch (NumberFormatException e) {
+            sender.sendMessage("§cInvalid number!");
+            return true;
+        }
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+            case "x":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0) {
+                    sender.sendMessage("§cCannot divide by zero!");
+                    return true;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                sender.sendMessage("§cUnknown operator! Please use +, -, *, or /");
+                return true;
+        }
+
+        sender.sendMessage("§aResult: §e" + result);
+        return true;
+    }
+}
